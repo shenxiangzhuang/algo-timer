b3ab81b05b9de483b86ef2904b3b8d786d08a610
shenxiangzhuang <1021550072@qq.com>
2019-11-23 14:07:16 +0800

    finished

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="algo-timer",
-    version="0.0.2",
+    version="0.0.3",
     author="Xiangzhuang Shen",
     author_email="datahonor@gmail.com",
     description="An easy-to-use algorithms timer.",
